87d97d3dad82b74431ad16cb72eb1c5e8431d67c
Даниил Попугаев <daniilapopugaev@yandex.ru>
2023-03-22 16:13:24 +0300

    Добавьте файлы проекта.

---
+++ ReadFile/ReadFile.cpp
@@ -0,0 +1,28 @@
+﻿#include <fstream>
+#include <iostream>
+#include <string>
+#include <string.h>
+#include <Windows.h>
+
+using namespace std;
+
+int main()
+{   
+    setlocale(LC_ALL, "Russian");
+    ifstream fin;
+    fin.open("file.txt");
+    string strh;
+    string str;
+
+    while (!fin.eof()) {
+        getline(fin, strh);
+        str += strh;
+        if (!fin.eof()) {
+            str += '\n';
+        }
+    }
+
+    cout << str;
+    fin.close();
+    return 0;
+}
